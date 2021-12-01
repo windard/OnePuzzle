24d38b8cc799955becbf961524f3fdfe5ebe2fb1
Wenqiang Yang <windard@qq.com>
2021-12-01 10:12:24 +0800

    create codecov.yml and fix Qt core dump directly

--- tests/test_solver.py
+++ tests/test_solver.py
@@ -6,6 +6,21 @@ from one_puzzle.solver import Solver
 
 
 class TestSolver(object):
+    def test_solver(self):
+        assert Solver(nox=True)
+        assert Solver(nox=True, color=True)
+
+    def test_solve(self, capsys):
+        solver = Solver(nox=True)
+        captured = capsys.readouterr()
+
+        solver.multi_solve()
+        out1 = captured.out
+
+        solver.multi_solve()
+        out2 = captured.out
+        assert out1 == out2
+
     def test_get_around(self):
         block = Block(2, 3)
         surrounds = Solver.get_around(block)
@@ -14,11 +29,10 @@ class TestSolver(object):
 
     def test_validate_trap(self):
         blocks = [Block(2, 3), Block(2, 4), Block(3, 3), Block(1, 3), Block(2, 2)]
-        solver = Solver()
 
-        assert solver.validate_trap([])
-        assert solver.validate_trap(blocks)
-        assert not solver.validate_trap([Block(2, 3), Block(2, 4)])
+        assert Solver.validate_trap([])
+        assert Solver.validate_trap(blocks)
+        assert not Solver.validate_trap([Block(2, 3), Block(2, 4)])
 
     def test_get_available_entities(self):
         blocks = {Block(1, 1), Block(1, 2), Block(1, 3), Block(2, 1), Block(2, 2), Block(2, 3)}
